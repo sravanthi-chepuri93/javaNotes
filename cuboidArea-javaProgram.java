ad6c7d7ca3cc903bfc3a479bcb27afd8f809ac40
sravanthi-chepuri93 <110753031+sravanthi-chepuri93@users.noreply.github.com>
2023-05-19 23:41:43 -0400

    Update cuboidArea-javaProgram.java

--- cuboidArea-javaProgram.java
+++ cuboidArea-javaProgram.java
@@ -11,17 +11,11 @@ public class Main {
         l = sc.nextInt();
         b = sc.nextInt();
         h = sc.nextInt();
-
-
-
         int i = (l * h) + (b * h) + (b * l);
         int cubeArea = 2*i;
         int cubeVolume = l * b * h;
 
         System.out.println("cube total area is "+cubeArea);
         System.out.println("cube volume is "+cubeVolume);
-
-
-
     }
 }
