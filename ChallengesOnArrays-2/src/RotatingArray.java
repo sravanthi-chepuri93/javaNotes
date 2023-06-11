157dcd9c98d87cec5e926510569de315fd33f716
sravanthi-chepuri93 <sravanthi.chepuri93@gmail.com>
2023-06-11 00:11:26 -0400

    RotatingAnArray

---
+++ ChallengesOnArrays-2/src/RotatingArray.java
@@ -0,0 +1,20 @@
+import java.lang.*;
+public class RotatingArray {
+    public static void main(String[] args){
+        int A[]= {3,6,4,7,8,9,2,1};
+        for(int x: A){
+            System.out.print(x+",");
+        }
+        System.out.println("");
+        int temp = A[0];
+        for(int i=1;i<A.length;i++){
+            A[i-1]=A[i];
+        }
+        A[A.length-1] = temp;
+        System.out.println(" after rotating an array");
+        for(int x: A){
+            System.out.print(x+",");
+        }
+
+    }
+}
