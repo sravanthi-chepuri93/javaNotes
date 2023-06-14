1a39632232d4cc98efb9ac0bad1c95fecf569e19
sravanthi-chepuri93 <sravanthi.chepuri93@gmail.com>
2023-06-14 17:10:56 -0400

    copying Array

---
+++ ChallengesOnArrays-2/src/copyArray.java
@@ -0,0 +1,19 @@
+public class copyArray {
+    public static void main(String[] args){
+        int A[] = {8,6,10,9,2,15,7,13,14,11};
+        int B[] = new int[10];
+
+
+        for(int i=0; i<A.length;i++){
+            B[i] = A[i];
+        }
+
+        for( int i=0; i<B.length;i++){
+            System.out.println(B[i]);
+        }
+
+
+
+    }
+
+}
